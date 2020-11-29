c3ef6804749badf5c3142310b015fa0d80e5badb
djaldave <djaldave@gmail.com>
2020-11-29 14:28:36 +0800

    first styling and layouting steps

--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,38 @@ import "question.dart";
 void main()=>runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-  State createState()=>_MyAppState();
+  State createState(){
+    return _MyAppState();
+  }
 }
 class _MyAppState extends State {
-  var _questIndex=0;
-  var _quest =[
-      "color?",
-      "fruit?",
-      "food",
-    ];
-  void _questButt(){
+  var _questionIndex =0;
+  var questions=[
+    "fruits?",
+    "colors?",
+    "number?"
+  ];
+  void answerQuestion(){
     setState((){
-      _questIndex = _questIndex + 1;
-      if(_questIndex==_quest.length){
-        _questIndex =0;
+      _questionIndex = _questionIndex +1;
+      if(_questionIndex == questions.length){
+        _questionIndex=0;
       }
     });
   }
   build(context){
     return MaterialApp(
       home:Scaffold(
-        appBar: AppBar(
-          title: Text("Al Dave"),
+        appBar:AppBar(
+          title: Text("App"),
         ),
-        body:Column(
+        body: Column(
           children:[
-            Question(_quest[_questIndex]),
-            RaisedButton(
-              child: Text("Question"),
-              onPressed: _questButt
-            ),
+            Question(questions[_questionIndex]),
+            RaisedButton(child: Text("Button"),onPressed:()=>answerQuestion()),
           ],
         ),
       ),
     );
-  }
+  }  
 }
