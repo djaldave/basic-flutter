33694bfba66f5e0ce78510f46e9415cbbe68b510
djaldave <djaldave@gmail.com>
2020-11-29 17:13:07 +0800

    mapping list to widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,57 @@
 import "package:flutter/material.dart";
-import "question.dart";
-import "answer.dart";
+import 'question.dart';
+import 'answer.dart';
+
+
 void main()=>runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   State createState(){
     return _MyAppState();
-  }
+  } 
 }
 class _MyAppState extends State {
-  var _questionIndex =0;
-  var questions=[
-    {
-      "questionText":"What is your favorite color",
-      "answers":["black","red","green","white"],
-  }
-  ];
+  var _questionIndex=0;
+  var _questions=[
+          {
+            "question":"What is your favorite Color? ",
+            "answer":["blue","black"]
+          },
+          {
+            "question":"What is your favorite Number?",
+            "answer":["1","2","3"]
+          },
+          {
+            "question":"What is your favorite Fruit?",
+            "answer":["mango","orange"]
+          },
+          {
+            "question":"what is your favorite Shape?",
+            "answer":["rectangle","circle"]
+          },
+        ];
   void _answerQuestion(){
     setState((){
       _questionIndex = _questionIndex +1;
-      if(_questionIndex == questions.length){
-        _questionIndex=0;
+      if(_questionIndex == _questions.length){
+        _questionIndex = 0;
       }
     });
   }
-  build(context){
+  build(context)  {
     return MaterialApp(
-      home:Scaffold(
-        appBar:AppBar(
-          title: Text("App"),
-        ),
+      home: Scaffold(
+        appBar: AppBar(title: Text("App")),
         body: Column(
           children:[
-            Question(questions[_questionIndex]),
-            Answer(_answerQuestion),
+            Question(_questions[_questionIndex]["question"]),
+            ...(_questions[_questionIndex]["answer"] as List<String>).map((answer){
+              return Answer(_answerQuestion,answer);
+            }),
           ],
         ),
       ),
     );
-  }  
+  }
 }
+
