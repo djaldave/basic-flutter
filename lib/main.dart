02443b9e5acf3b60c374beeb7b1359e093214b1d
djaldave <djaldave@gmail.com>
2020-11-29 19:55:07 +0800

    getters and else if

--- lib/main.dart
+++ lib/main.dart
@@ -11,47 +11,44 @@ class MyApp extends StatefulWidget {
 }
 class _MyAppState extends State {
   var _questionIndex=0;
-  var _totalScore=0;
+  int _totalScore=0;
   final _questions= const[
           {
             "question":"What is your favorite Color? ",
             "answer":
             [
-              {"text":"blue","score":"10"},
-              {"text":"black","score":"20"}
+              {"text":"blue","score":10},
+              {"text":"black","score":20}
             ]
           },
           {
             "question":"What is your favorite Number?",
             "answer":
             [
-              {"text":"1","score":"10"},
-              {"text":"2","score":"20"},
+              {"text":"1","score":10},
+              {"text":"2","score":20},
             ]
           },
           {
             "question":"What is your favorite Fruit?",
             "answer":
             [
-              {"text":"mango","score":"10"},
-              {"text":"orange","score":"20"}]
-          },
-          {
-            "question":"what is your favorite Shape?",
-            "answer":["rectangle","circle"]
+              {"text":"mango","score":10},
+              {"text":"orange","score":20}]
           },
         ];
   void _answerQuestion(int score){
     setState((){
       _questionIndex = _questionIndex +1;
-      _totalScore = _totalScore+ score;
+      _totalScore+= score;
+      print(_totalScore);
     });
   }
   build(context)  {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("App")),
-        body:_questionIndex < _questions.length? Quiz(questionIndex: _questionIndex,questions: _questions,answerQuestion: _answerQuestion):Result(),
+        body:_questionIndex < _questions.length? Quiz(questionIndex: _questionIndex,questions: _questions,answerQuestion: _answerQuestion):Result(_totalScore),
       ),
     );
   }
